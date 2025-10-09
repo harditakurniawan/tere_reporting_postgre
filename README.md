3baeae2c2e7b46f73a1d028bfed7425988a7f962
harditakurniawan <hardita.kurniawan@gmail.com>
2025-10-09 12:09:31 +0700

    feat: script for reproduce fact atp redeem

--- README.md
+++ README.md
@@ -1,23 +1,4 @@
-# Reporting Fact Detail (PostgreSQL)
-
-### Structure Folder
-```
-root/
-├── config/
-│   └── app_config.py
-├── database/
-│   └── postgres.py
-├── model/
-│   └── model.txt
-├── services/
-│   └── fact_detail.py
-├── utils/
-│   ├── datetime_utils.py
-│   ├── logger.py
-│   └── utils.py
-├── main.py
-└── .env
-```
+# Reporting Fact ATP Redeem (PostgreSQL)
 
 ### How to run:
 * Adjust env
@@ -25,5 +6,5 @@ root/
 * Create db and its tables (see schema in model.txt)
 * Activate virtual env (optional) and run python3 main.py
 * Input target date with format YYYY-MM-DD
-* Input file name (ex: fact_detail.dat)
+* Input file name (ex: fact_atp_redeem.dat)
 * Input exclude keyword (it's optional. just enter if you don't need it)
--- main.py
+++ main.py
@@ -25,6 +25,9 @@ def allowed_indihome_number(msisdn: str) -> bool :
 def is_null(value: any) -> bool :
     return pd.isna(value) or str(value).strip().lower() in ('', 'null', 'none', 'nat')
 
+def safe_get_field(line, fields, field_name, default=''):
+    return line[fields.index(field_name)] if field_name in fields and not is_null(line[fields.index(field_name)]) else default
+
 def initialize_db_connection(dbname: str, dbuser: str, dbpassword: str, dbhost: str, dbport: str) :
     try:
         conn = psycopg2.connect(
@@ -58,29 +61,13 @@ def convert_datetime(dt_str: str) -> datetime :
 def formatted_trx_date(dt_str: str) -> str :
     dt_obj = pd.to_datetime(str(dt_str).split("+")[0], format='%Y-%m-%d %H:%M:%S')
     dt_obj += pd.Timedelta(hours=7)
-    return dt_obj.strftime('%d/%m/%Y %H:%M')
+    return dt_obj.strftime('%Y%m%d%H%M%S')
 
-def validation_keyword_point_value_rule(total_redeem: int, poin_value: str, poin_redeemed: str, total_point=None):
-    result = 0
-
-    if total_point is not None:
-        result = total_point
-    elif not is_null(total_redeem):
-        result = total_redeem
-
-    if poin_value == 'Fixed':
-        result = poin_redeemed
-
-    elif poin_value == 'Flexible':
-        if result <= 0:
-            result = poin_redeemed
-
-    elif poin_value == 'Fixed Multiple':
-        if result > 0:
-            result = poin_redeemed
-
+def format_msisdn(msisdn: str) -> str :
+    if allowed_indihome_number(msisdn):
+        return msisdn
     else:
-        return 0
+        return f"62{msisdn}" if msisdn.startswith('8') else msisdn
 
 def write_ctl_file(filename: str, single_filename: str):
     with open(filename, "rb") as f:
@@ -95,85 +82,33 @@ def write_ctl_file(filename: str, single_filename: str):
 #                   SERVICE
 # ==============================================
 def get_records(connection, start_date: str, end_date: str, exclude_keywords: list[str], batch_size: int) :
-        raw_query = """
-            SELECT
-                trx_master.transaction_id,
-                trx_master.transaction_date,
-                trx_master.status AS status,
-                trx_master.origin AS origin,
-                trx_master.keyword AS keyword,
-                trx_master.keyword AS keyword_title,
-                trx_master.msisdn AS msisdn,
-                trx_master.channel_id AS channel_code,
-                trx_master.poin AS poin,
+    raw_query = """
+        SELECT *
+        FROM mongo.report_redeem_transaction
+        WHERE transaction_date >= %s
+        AND transaction_date < %s
+    """
 
-                COALESCE(tmd.program_name, '') AS program_name,
-                COALESCE(tmd.program_experience, '') AS program_experience,
-                COALESCE(tmd.poin_value, '') AS poin_value,
-                COALESCE(tmd.poin_redeemed, 0) AS poin_redeemed,
-                COALESCE(tmd.customer_value, NULL) AS cust_value,
-                COALESCE(tmd.start_period, NULL) AS start_date,
-                COALESCE(tmd.end_period, NULL) AS end_date,
-                COALESCE(tmd.merchant, '') AS merchant,
-                COALESCE(tmd.program_bersubsidi, NULL) AS subsidy,
-                COALESCE(tmd.channel_id, '') AS sms,
-                COALESCE(tmd.channel_id, '') AS umb,
-                COALESCE(tmd.total_redeem, NULL) AS total_redeem,
-                COALESCE(tmd.brand, '') AS subscriber_brand,
-                COALESCE(tmd.region, '') AS subscriber_region,
-                COALESCE(tmd.city, '') AS subscriber_branch,
-                COALESCE(tmd.tier_name, '') AS subscriber_tier,
-                COALESCE(tmd.voucher_code, '') AS voucher_code,
+    params = [start_date, end_date]
 
-                COALESCE(acc.user_name, '') AS created_by,
-                COALESCE(lov1.set_value, '') AS program_owner,
-                COALESCE(lov2.set_value, '') AS lifestyle,
-                COALESCE(lov2.set_value, '') AS category,
-                COALESCE(loc.name, '') AS detail_program_owner,
-                COALESCE(loc_prefix.area, '') AS program_regional,
-                COALESCE(merchant.merchant_name, '') AS merchant_name,
-                SUBSTRING(trx_master.msisdn FROM 3 FOR 6) AS msisdn_prefix
-            FROM (
-                SELECT *
-                FROM mongo.transaction_master
-                WHERE transaction_date >= %s
-                AND transaction_date < %s
-                AND status = 'Success'
-                AND origin ~ '^redeem'
-        """
+    if exclude_keywords:
+        placeholders = ', '.join(['%s'] * len(exclude_keywords))
+        raw_query += f" AND keyword NOT IN ({placeholders})"
+        params.extend(exclude_keywords)
 
-        params = [start_date, end_date]
+    cursor = connection.cursor(name='fact_atp_redeem_cursor')
+    cursor.itersize = batch_size
+    cursor.execute(raw_query, params)
+    
+    while True:
+        batch = cursor.fetchmany(batch_size)
+        if not batch:
+            Logger("info", "No more data to fetch.")
+            break
+        Logger("info", f"Fetched batch with {len(batch)} records.")
+        yield pd.DataFrame(batch, columns=[desc[0] for desc in cursor.description])
 
-        if exclude_keywords:
-            placeholders = ', '.join(['%s'] * len(exclude_keywords))
-            raw_query += f" AND keyword NOT IN ({placeholders})"
-            params.extend(exclude_keywords)
-
-        raw_query += """
-            ) AS trx_master
-            LEFT JOIN mongo.transaction_master_detail tmd ON tmd.master_id = trx_master.transaction_id
-            LEFT JOIN mongo.accounts acc ON trx_master.created_by = acc._id
-            LEFT JOIN mongo.lovs lov1 ON tmd.program_owner = lov1._id
-            LEFT JOIN mongo.lovs lov2 ON tmd.program_experience = lov2._id
-            LEFT JOIN mongo.merchantv2 merchant ON tmd.merchant = merchant._id
-            LEFT JOIN mongo.locations loc ON tmd.program_owner_detail = loc._id
-            LEFT JOIN mongo.locationprefixes loc_prefix ON SUBSTRING(trx_master.msisdn FROM 3 FOR 6) = loc_prefix.prefix;
-        """
-
-        cursor = connection.cursor(name='fact_detail_cursor')
-        cursor.itersize = batch_size
-        cursor.execute(raw_query, params)
-        
-
-        while True:
-            batch = cursor.fetchmany(batch_size)
-            if not batch:
-                Logger("info", "No more data to fetch.")
-                break
-            Logger("info", f"Fetched batch with {len(batch)} records.")
-            yield pd.DataFrame(batch, columns=[desc[0] for desc in cursor.description])
-
-        cursor.close()
+    cursor.close()
 
 def main():
     try:
@@ -217,53 +152,41 @@ def main():
                     batch_numpy = batches.to_numpy()
                     
                     for line in batch_numpy:
-                        transaction_date = ""
-                        if line[fields.index("transaction_date")]:
-                            transaction_date_unformatted = convert_datetime(f"{line[fields.index("transaction_date")]}".replace(" ", "T").split(".")[0])
-                            transaction_date = f"{formatted_trx_date(transaction_date_unformatted)}" or ""
-
-                        start_date = ""
-                        if not is_null(line[fields.index("start_date")]):
-                            start_date_unformatted = convert_datetime(f"{line[fields.index("start_date")]}".replace(" ", "T").split(".")[0])
-                            start_date = f"{formatted_trx_date(start_date_unformatted)}" or ""
-
-                        end_date = ""
-                        if not is_null(line[fields.index("end_date")]):
-                            end_date_unformatted = convert_datetime(f"{line[fields.index("end_date")]}".replace(" ", "T").split(".")[0])
-                            end_date = f"{formatted_trx_date(end_date_unformatted)}" or ""
+                        execution_date = ""
+                        if line[fields.index("execution_date")]:
+                            execution_date_unformatted = convert_datetime(f"{line[fields.index("execution_date")]}".replace(" ", "T").split(".")[0])
+                            execution_date = f"{formatted_trx_date(execution_date_unformatted)}" or ""
 
                         allowed_IH = f"{allowed_indihome_number(line[fields.index("msisdn")])}".lower()
-                        
-                        poin_redeemed = line[fields.index('poin_redeemed')]
-                        total_redeem = line[fields.index('total_redeem')]
-                        poin_value = line[fields.index('poin_value')]
+                        msisdn_formatted = format_msisdn(line[fields.index("msisdn")])
 
                         to_write = (
-                            f"{transaction_date}|"
-                            f"{line[fields.index('msisdn')]}|"
-                            f"{line[fields.index('keyword')]}|"
-                            f"{line[fields.index('program_name')]}|"
-                            f"{line[fields.index('program_owner')]}|"
-                            f"{line[fields.index('detail_program_owner')]}|"
-                            f"{line[fields.index('created_by')]}|"
-                            f"{line[fields.index('lifestyle')]}|"
-                            f"{line[fields.index('category')]}|"
-                            f"{line[fields.index('keyword_title')]}|"
-                            f"{line[fields.index('sms')]}|"
-                            f"{line[fields.index('umb')]}|"
-                            f"{ validation_keyword_point_value_rule(total_redeem, poin_value, poin_redeemed) }|"
-                            f"{line[fields.index('subscriber_brand') or '']}|"
-                            f"{line[fields.index('program_regional')]}|"
-                            f"{line[fields.index('cust_value')]}|"
-                            f"{start_date}|"
-                            f"{end_date}|"
-                            f"{line[fields.index('merchant_name')]}|"
-                            f"{line[fields.index('subscriber_region')]}|"
-                            f"{line[fields.index('subscriber_branch')]}|"
-                            f"{line[fields.index('channel_code')]}|"
-                            f"{line[fields.index('subsidy')]}|"
-                            f"{line[fields.index('subscriber_tier')]}|"
-                            f"{line[fields.index('voucher_code')]}|"
+                            f"{safe_get_field(line, fields, 'transaction_id')}|"
+                            f"{safe_get_field(line, fields, 'keyword')}|"
+                            f"{safe_get_field(line, fields, 'keyword_title')}|"
+                            f"{safe_get_field(line, fields, 'execution_type')}|"
+                            f"{safe_get_field(line, fields, 'product_id')}|"
+                            f"{safe_get_field(line, fields, 'period1')}|"
+                            f"{safe_get_field(line, fields, 'period2')}|"
+                            f"{safe_get_field(line, fields, 'subscriber_id')}|"
+                            f"{msisdn_formatted}|"
+                            f"{safe_get_field(line, fields, 'return_value')}|"
+                            f"{execution_date}|"
+                            f"{safe_get_field(line, fields, 'channel_code')}|"
+                            f"{safe_get_field(line, fields, 'transaction_status')}|"
+                            f"{safe_get_field(line, fields, 'trdm_last_act')}|"
+                            f"{safe_get_field(line, fields, 'trdm_act_status')}|"
+                            f"{safe_get_field(line, fields, 'trdm_evd_id')}|"
+                            f"{safe_get_field(line, fields, 'trdm_flag_kirim')}|"
+                            f"{safe_get_field(line, fields, 'trdm_geneva_exec')}|"
+                            f"{safe_get_field(line, fields, 'trdm_keyword')}|"
+                            f"{safe_get_field(line, fields, 'trdm_tgl_kirim')}|"
+                            f"{safe_get_field(line, fields, 'channel_transaction_id')}|"
+                            f"{safe_get_field(line, fields, 'card_type')}|"
+                            f"{safe_get_field(line, fields, 'brand')}|"
+                            f"{safe_get_field(line, fields, 'subscriber_region')}|"
+                            f"{safe_get_field(line, fields, 'subscriber_branch')}|"
+                            f"{safe_get_field(line, fields, 'lacci')}|"
                             f"{allowed_IH}"
                         )
 
@@ -275,7 +198,7 @@ def main():
             write_ctl_file(filename, single_filename)
 
         except Exception as e:
-            Logger("error", f"ERROR - FactDetailService error: {e} \n")
+            Logger("error", f"ERROR - FactAtpRedeemService error: {e} \n")
             Logger("error", f"{traceback.format_exc()}")
 
     except ValueError as e:
